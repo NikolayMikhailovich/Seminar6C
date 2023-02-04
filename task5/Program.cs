96e3c9fd5844cfb44121982a2e8470a6ec11d373
nikolays <sidorovkolya91@gmail.com>
2023-02-04 12:14:41 +0300

    Напишите программу, которая будет создавать копию заданного массива //с помощью поэлементного копирования.

---
+++ task5/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 4: Напишите программу, которая будет создавать копию заданного массива 
+//с помощью поэлементного копирования.
+
+int[] CreateArray()
+{
+  int[] array = new int[6];
+  Random rnd = new Random();
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = rnd.Next(1, 11);
+  }
+  return array;
+}
+void PrintArray(int[] array, string msg)
+{
+  Console.WriteLine($"{msg}:");
+  foreach (int item in array)
+  {
+    Console.Write(item + " ");
+  }
+  Console.WriteLine();
+}
+int[] CopyArray(int[] array)
+{
+  int[] result = new int[array.Length];
+  for (int i = 0; i < array.Length; i++)
+  {
+    result[i] = array[i];
+  }
+  return result;
+}
+int[] baseArray = CreateArray();
+PrintArray(baseArray, "Базовый массив");
+int[] copiedArray = CopyArray(baseArray);
+PrintArray(copiedArray, "Скопированный массив");
